276024b5a57b5ad081cac7b955430032b8378d60
Sandeep Rohilla <81100360+sr-dev-coder@users.noreply.github.com>
2023-02-06 23:16:14 +0530

    Added Header in project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Header } from '@/src/components'
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">
-    Hello world!
-</h1>
+  return <>
+    <Header />
+  </>
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
+
+export const Header = () => {
+  return <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5">
+    
+    {/* Logo  */}
+    <div className="relative flex items-center h-10 cursor-pointer my-auto">
+        <Image 
+            src="/logo.png"
+            fill 
+            alt={''}      
+            style={{objectFit:"contain", objectPosition: "left"}}
+        />    
+    </div>
+
+    {/* Search Bar */}
+    <div className="flex items-center border rounded-full">
+        <input className="pl-5 bg-transparent outline-none flex-grow " type="text" placeholder="Start your search" />
+        <MagnifyingGlassIcon className="h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer" />
+    </div>
+
+  </header>
+}
