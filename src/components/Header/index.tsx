532d984b618ba78bac58c88ec48b91a14d1becd9
Sandeep Rohilla <81100360+sr-dev-coder@users.noreply.github.com>
2023-02-07 23:42:39 +0530

    Added responsive feature

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,10 +2,13 @@ import Image from 'next/image'
 import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
 
 export const Header = () => {
-  return <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5">
+  return (
+    <header 
+        className="sticky top-0 z-50 md:grid md:grid-cols-3 bg-white shadow-md px-6 pt-3.5"
+    >
     
     {/* Logo  */}
-    <div className="relative flex items-center h-10 cursor-pointer my-auto">
+    <div className="relative md:flex items-center h-10 cursor-pointer my-auto hidden">
         <Image 
             src="/logo.png"
             fill 
@@ -15,10 +18,10 @@ export const Header = () => {
     </div>
 
     {/* Search Bar */}
-    <div className="flex items-center border rounded-full"> 
-        <input className="pl-5 bg-transparent outline-none flex-grow " type="text" placeholder="Start your search" />
-        <MagnifyingGlassIcon className="h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer" />
+    <div className="flex items-center shadow-lg border rounded-full"> 
+        <input className="pl-5 h-14  bg-transparent outline-none flex-grow " type="text" placeholder="Start your search" />
+        <MagnifyingGlassIcon className="h-10 mr-2 bg-red-400 text-white rounded-full p-2 cursor-pointer" />
     </div>
 
-  </header>
+  </header>)
 }
