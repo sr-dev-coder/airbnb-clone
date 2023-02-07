3fbd119abfe976dab1d709c3ac67e01982403d14
Sandeep Rohilla <81100360+sr-dev-coder@users.noreply.github.com>
2023-02-08 01:30:09 +0530

    Added Right section in header (AirbnbClone)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
-import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
+import { MagnifyingGlassIcon, UserCircleIcon, Bars3Icon } from '@heroicons/react/24/solid';
 
 export const Header = () => {
   return (
     <header 
-        className="sticky top-0 z-50 md:grid md:grid-cols-3 bg-white shadow-md px-6 pt-3.5"
+        className="sticky top-0 z-50 md:grid md:grid-cols-3 bg-white px-6 pt-3.5 md:px-20 md:py-3 border-b border-slate-100"
     >
     
     {/* Logo  */}
@@ -18,9 +18,18 @@ export const Header = () => {
     </div>
 
     {/* Search Bar */}
-    <div className="flex items-center shadow-lg border rounded-full"> 
-        <input className="pl-5 h-14  bg-transparent outline-none flex-grow " type="text" placeholder="Start your search" />
-        <MagnifyingGlassIcon className="h-10 mr-2 bg-red-400 text-white rounded-full p-2 cursor-pointer" />
+    <div className="flex items-center shadow-lg md:shadow-none border md:border-2 md:w-96 rounded-full"> 
+        <input className="pl-5 h-14 md:h-10 bg-transparent outline-none flex-grow " type="text" placeholder="Start your search" />
+        <MagnifyingGlassIcon className="h-10 md:h-8 mr-2 md:mr-1 bg-red-400 text-white rounded-full p-2 cursor-pointer" />
+    </div>
+
+    {/* Right Options */}
+    <div className="flex items-center justify-end">
+        <a href="#" className="mr-4">Airbnb your home</a>
+        <div className="flex items-center border rounded-full cursor-pointer px-2 py-1">
+            <Bars3Icon className="h-6 mr-2 text-slate-500" />
+            <UserCircleIcon className="h-8 text-slate-500" />
+        </div>
     </div>
 
   </header>)
